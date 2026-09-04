46a24c981110fe46a92283aab01d91b5a130397d
FA-AlOtaibi <31261131+FA-AlOtaibi@users.noreply.github.com>
2026-09-04 18:04:40 +0300

    Add Object Studio app entry point

---
+++ ObjectStudio/ObjectStudio/ObjectStudioApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct ObjectStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
+    }
+}
